0fda6d1d277ed75b5f80c6c873cb8462a41be6da
Roman Toritsyn <devroman@mlsworldwide.com>
2023-08-28 11:39:03 +0300

    feat: alowing negative numbers for create sensor coordinates

--- src/scenes/CreateSensor/validation.schema.tsx
+++ src/scenes/CreateSensor/validation.schema.tsx
@@ -4,13 +4,7 @@ export type TFormData = Yup.InferType<typeof validationSchema>;
 
 export const validationSchema = Yup.object({
   name: Yup.string().required("Name is required"),
-  x: Yup.number()
-    .required("Coordinate X is required")
-    .min(0, "Coordinate X must be greater than or equal to 0"),
-  y: Yup.number()
-    .required("Coordinate Y is required")
-    .min(0, "Coordinate Y must be greater than or equal to 0"),
-  z: Yup.number()
-    .required("Coordinate Z is required")
-    .min(0, "Coordinate Z must be greater than or equal to 0"),
+  x: Yup.number().required("Coordinate X is required"),
+  y: Yup.number().required("Coordinate Y is required"),
+  z: Yup.number().required("Coordinate Z is required"),
 });
